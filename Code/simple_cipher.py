8aadf7b01b3fd6da98840e1b4b24690e698b00b9
rahuldan <rahul02013@gmail.com>
2016-09-29 01:28:08 +0530

    Added Simple Cipher
    Added code of simple cipher and a test run result

---
+++ Code/simple_cipher.py
@@ -0,0 +1,53 @@
+"""
+Created on Wed Sep 28 00:04:00 2016
+
+@author: Rahul Dandwate
+"""
+import cv2
+import numpy as np
+import random
+
+N = 50                                                  #size of one time pad
+l = 2                                                   #lower limit of random integer
+h = 257                                                 #upper limit of random integer
+k = np.random.random_integers(l, high = h, size = N)    #random key set
+R = 3.9                                                 #parameter for logistic map
+
+img = cv2.imread('Test image.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+img_shape = np.shape(img)
+enc_img = np.zeros(shape = img_shape)
+dec_img = np.zeros(shape = img_shape)
+
+def logistic_map(r, x):
+    return r*x*(1 - x)
+
+def encryption():
+    idx = 0
+    for i in range(0, img_shape[0]):
+        for j in range(0, img_shape[1]):
+            X = 1 / k[idx]
+            idx = (idx + 1) % N
+            t = 16 + k[idx]
+            for a in range(0, t):
+                X = logistic_map(R, X)
+            enc_img[i][j] = (img[i][j] + int(1 / X)) % 256
+
+def decryption():
+    idx = 0
+    for i in range(0, img_shape[0]):
+        for j in range(0, img_shape[1]):
+            X = 1 / k[idx]
+            idx = (idx + 1) % N
+            t = 16 + k[idx]
+            for a in range(0, t):
+                X = logistic_map(R, X)
+            dec_img[i][j] = (enc_img[i][j] - int(1 / X)) % 256
+
+encryption()
+decryption()
+cv2.imshow('original', img)            #displays image
+cv2.imshow('encrypted image', enc_img)
+cv2.imshow('decrypted image', dec_img)
+cv2.waitKey()
