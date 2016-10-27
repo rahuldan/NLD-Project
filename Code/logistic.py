5bd671e8ccaa475b8f8d2bad802541658a33dd23
rahuldan <rahul02013@gmail.com>
2016-10-27 23:05:29 +0530

    Some Changes

--- Code/logistic.py
+++ Code/logistic.py
@@ -18,9 +18,13 @@ X0 = 0.2323232                                              #initial condition
 
 iter = 60000                                                #for statistical analysis
 stat = np.zeros(S)
+num_val = 200
+stat2 = np.zeros(num_val)
 
-img = cv2.imread("image file name")                              #Enter the name of the image file
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img = cv2.imread("square.jpg")                              #Enter the name of the image file
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+img = np.ones(shape = (S, S), dtype = np.uint8)
 
 img_shape = np.shape(img)
 enc_img = np.zeros(shape = img_shape, dtype = np.uint16)
@@ -65,6 +69,22 @@ def logistic_stat():
         if bin_n < 256 and bin_n >= 0:
             stat[bin_n] += 1
 
+b = np.linspace(Xmin, Xmax, num = num_val)
+
+def logistic_stat2():
+    iter = 10000
+    for i in range(0, num_val):
+        stat2[i] = b[i]
+        for j in range(0, iter):
+            stat2[i] = logistic_map(stat2[i])
+
+"""logistic_stat2()
+plt.plot(b, stat2, label = 'R = 4.0')
+plt.xlabel('Initial Condition')
+plt.ylabel('Mapped Value after 200 Iterations')
+plt.legend(loc = 'upper right')
+plt.show()
+
 logistic_stat()
 a = np.arange(S)
 
@@ -72,10 +92,13 @@ plt.xlabel('Index of bin')
 plt.ylabel('Frequency')
 plt.scatter(a, stat, label = 'x = 0.2323232, R = 4.0')
 plt.legend(loc = 'upper right')
-plt.show()
+plt.show()"""
 
 encryption()
 decryption()
+
+plt.hist(np.ravel(enc_img))
+plt.show()
 
 cv2.imshow('original', img)                                     #displays image
 cv2.imshow('encrypted image', enc_img)
